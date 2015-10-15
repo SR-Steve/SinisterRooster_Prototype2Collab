7e4dc3cdd40b8e3152989fda6b058f9d1e227131
SR-Steve <smg337@scarletmail.rutgers.edu>
2015-10-14 22:00:10 -0400

    Jump Fix
    Most if not all of us participated in this going down over Skype.
    Jake's Jump code was reconciled with the Player GameObject.  Colliders'
    friction was reduced to 0 (a quick fix for sticking to walls with no
    consequences, yet).

--- Prototype2Collab/Assets/Scripts/JumpController.cs
+++ Prototype2Collab/Assets/Scripts/JumpController.cs
@@ -10,7 +10,7 @@ using System.Collections;
 namespace JakeHohing
 {
 
-    public class Moveleftright : MonoBehaviour
+    public class JumpController : MonoBehaviour
     {
         Rigidbody2D bod;
 
