83ced81e4cde0393d073cbc7bb828a9e5cf84250
AdamThai <adamsapp1e@hotmail.com>
2015-10-20 22:54:04 -0400

    Prompts
    X-button prompt will appear above Anais when near a moveable block or
    ladder. Left and right button will appear to side Anais. Sill having
    some problems with the X-button not disappearing after pressing the x
    button and the prompts flipping if Anais turns around.

---
+++ Prototype2Collab/Assets/Prompt/eprompt.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eprompt : MonoBehaviour{
+
+	public SpriteRenderer ebutton; //e-button prompt -Adam Thai
+	public bool touching;
+	
+	void Start()
+	{
+		
+		ebutton = GetComponent<SpriteRenderer> ();
+		ebutton.enabled=false;
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D col) 
+	{
+		if (col.gameObject.name == "front") 
+			ebutton.enabled = true;	//e-button prompt will appear over player -Adam Thai 
+	}
+	
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.gameObject.name == "front")
+			ebutton.enabled=false; // if too far from "GameObject" e-button prompt will disappear -Adam Thai
+	}
+	
+	void Update()
+	{
+		
+	}
+	
+	
+}
